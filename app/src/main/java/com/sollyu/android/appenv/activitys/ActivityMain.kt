c7709cf45e073329325375766a978aaf001980ef
sollyu <sollyu@qq.com>
2017-11-21 10:45:30 +0800

    no message

--- app/src/main/java/com/sollyu/android/appenv/activitys/ActivityMain.kt
+++ app/src/main/java/com/sollyu/android/appenv/activitys/ActivityMain.kt
@@ -1,20 +1,38 @@
 package com.sollyu.android.appenv.activitys
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
+import android.view.*
 import com.sollyu.android.appenv.R
+import com.sollyu.android.appenv.commons.Application
+import com.sollyu.android.appenv.events.EventSample
+import com.sollyu.android.libsuperuser.Shell
+import com.sollyu.android.option.item.OptionItemView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_activity_main.*
+import kotlinx.android.synthetic.main.content_activity_main.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.xutils.view.annotation.ViewInject
+import org.xutils.x
+import java.util.*
 
-class ActivityMain : ActivityBase(), NavigationView.OnNavigationItemSelectedListener {
+class ActivityMain : ActivityBase(), NavigationView.OnNavigationItemSelectedListener, SwipeRefreshLayout.OnRefreshListener  {
 
     companion object {
         fun launch(activity: Activity) {
@@ -23,24 +41,44 @@ class ActivityMain : ActivityBase(), NavigationView.OnNavigationItemSelectedList
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val recyclerViewAdapter = RecyclerViewAdapter()
+    private val linearLayoutManager by lazy { LinearLayoutManager(activity) }
+
+    override fun onInitView() {
+        super.onInitView()
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        x.view().inject(activity)
+        EventBus.getDefault().register(this)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = recyclerViewAdapter
+        recyclerView.setItemViewCacheSize(1000)
     }
 
+    override fun onInitListener() {
+        super.onInitListener()
+        nav_view.setNavigationItemSelectedListener(this)
+        swipeRefreshLayout.setOnRefreshListener(this)
+    }
+
+    /**
+     * 初始化结束
+     */
+    override fun onInitDone() {
+        super.onInitDone()
+        EventBus.getDefault().postSticky(EventSample(EventSample.TYPE.MAIN_REFRESH))
+    }
+
+    /**
+     * 返回键被按下
+     */
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
@@ -48,6 +86,16 @@ class ActivityMain : ActivityBase(), NavigationView.OnNavigationItemSelectedList
             super.onBackPressed()
         }
     }
+
+    /**
+     * 本界面被销毁
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        EventBus.getDefault().unregister(this)
+    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,5 +123,76 @@ class ActivityMain : ActivityBase(), NavigationView.OnNavigationItemSelectedList
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return false
+    }
+
+    /**
+     * 收到消息事件
+     */
+    @SuppressLint("SetTextI18n")
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    fun onRefresh(eventSample: EventSample) {
+        when (eventSample.eventTYPE) {
+            EventSample.TYPE.MAIN_REFRESH -> {
+                swipeRefreshLayout.isRefreshing = false
+                val installPackage = Application.Instance.packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+                recyclerViewAdapter.displayAppList.clear()
+                recyclerViewAdapter.displayAppList.addAll(installPackage)
+                recyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    /**
+     * 下拉刷新
+     */
+    override fun onRefresh() {
+        EventBus.getDefault().postSticky(EventSample(EventSample.TYPE.MAIN_REFRESH))
+    }
+
+
+    /**
+     * List Item
+     */
+    inner class RecyclerViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+
+        @ViewInject(R.id.titleName)
+        var tvTitleName: OptionItemView? = null
+
+        init {
+            x.view().inject(this, itemView)
+            itemView?.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+        }
+    }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewHolder>() {
+
+        val displayAppList = LinkedList<ApplicationInfo>()
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerViewHolder {
+            val view = LayoutInflater.from(activity).inflate(R.layout.item_listview, parent, false)
+            val typedValue = TypedValue()
+            activity.theme.resolveAttribute(R.attr.selectableItemBackground, typedValue, true)
+            view.setBackgroundResource(typedValue.resourceId)
+            return RecyclerViewHolder(view)
+        }
+
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder?, position: Int) {
+
+            val applicationInfo = displayAppList[position]
+            holder?.tvTitleName?.setLeftText(applicationInfo.loadLabel(packageManager))
+
+            // holder?.tvTitleName?.leftCheckBox?.visibility = View.VISIBLE
+            // holder?.tvTitleName?.isChecked = true
+
+        }
+
+        override fun getItemCount(): Int {
+            return displayAppList.size
+        }
     }
 }
---
+++ app/src/main/java/com/sollyu/android/appenv/events/EventSample.kt
@@ -0,0 +1,12 @@
+package com.sollyu.android.appenv.events
+
+/**
+ * 作者：sollyu
+ * 时间：2017/10/30
+ * 说明：
+ */
+class EventSample(val eventTYPE: TYPE) {
+    enum class TYPE {
+        MAIN_REFRESH
+    }
+}
