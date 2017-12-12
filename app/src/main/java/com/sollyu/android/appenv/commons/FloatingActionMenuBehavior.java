801652ff983ea4d8d0cd2e0ea840652d97cafaff
sollyu <sollyu@qq.com>
2017-12-12 10:35:01 +0800

    chage detail activity.

---
+++ app/src/main/java/com/sollyu/android/appenv/commons/FloatingActionMenuBehavior.java
@@ -0,0 +1,39 @@
+package com.sollyu.android.appenv.commons;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Andrey Kalashnikov on 08.10.16.
+ * KSystems
+ * andrey.k@ksyste.ms
+ */
+@SuppressWarnings("unused")
+public class FloatingActionMenuBehavior extends CoordinatorLayout.Behavior<com.github.clans.fab.FloatingActionMenu> {
+
+    public FloatingActionMenuBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, com.github.clans.fab.FloatingActionMenu child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, com.github.clans.fab.FloatingActionMenu child, View dependency) {
+        float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
+        child.setTranslationY(translationY);
+        return true;
+    }
+
+    @Override
+    public void onDependentViewRemoved(CoordinatorLayout parent, com.github.clans.fab.FloatingActionMenu child, View dependency) {
+        float translationY = Math.max(0, dependency.getTranslationY() - dependency.getHeight());
+        child.setTranslationY(translationY);
+        super.onDependentViewRemoved(parent, child, dependency);
+    }
+}
