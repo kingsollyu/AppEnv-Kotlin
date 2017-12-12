d38a94445c0777eaaa6c91f832b018ef5d47ba1f
sollyu <sollyu@qq.com>
2017-12-12 10:42:52 +0800

    add copyright -> lgpl v3.0

--- app/src/main/java/com/sollyu/android/appenv/commons/XposedEntry.kt
+++ app/src/main/java/com/sollyu/android/appenv/commons/XposedEntry.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2017 Sollyu <https://www.sollyu.com/>
+ *
+ * Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+ *
+ * This version of the GNU Lesser General Public License incorporates the terms and conditions of version 3 of the GNU General Public License, supplemented by the additional permissions listed below.
+ */
+
 package com.sollyu.android.appenv.commons
 
 import android.net.wifi.WifiInfo
